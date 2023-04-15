398271b893b19414280d0bfcb00968bf45116699
zabryant89 <zachary.a.bryant1@gmail.com>
2023-04-14 22:05:39 -0400

    WIP Checkout Functions
    I've done some tinkering (I have a working version but it's very barebones and unlikely to work with the final design).
    
    Consider this the WIP version that includes many different functionality types and options.  Bare minimum HTML alterations (mostly to display outputs and compare to console)

--- foodorderingapp/src/App.js
+++ foodorderingapp/src/App.js
@@ -1,23 +1,34 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Checkout from './checkout.js';
 
 function App() {
+  const [showCheckout, setShowCheckout] = useState(false);
+
+  console.log("running App() from App.js")
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {showCheckout ? (
+        <Checkout />
+      ) : (
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+
+          <button onClick={() => setShowCheckout(true)}>Click for checkout</button>
+        </header>
+      )}
     </div>
   );
 }
