08439bee9146bd38568bf3aded475b99f817ca13
zabryant89 <zachary.a.bryant1@gmail.com>
2023-04-15 09:30:30 -0400

    Revert "WIP Checkout Functions"
    This reverts commit 398271b893b19414280d0bfcb00968bf45116699.

--- foodorderingapp/src/App.js
+++ foodorderingapp/src/App.js
@@ -1,34 +1,23 @@
-import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Checkout from './checkout.js';
 
 function App() {
-  const [showCheckout, setShowCheckout] = useState(false);
-
-  console.log("running App() from App.js")
   return (
     <div className="App">
-      {showCheckout ? (
-        <Checkout />
-      ) : (
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-
-          <button onClick={() => setShowCheckout(true)}>Click for checkout</button>
-        </header>
-      )}
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
--- foodorderingapp/src/checkout.js
+++ foodorderingapp/src/checkout.js
@@ -1,88 +1,41 @@
 //Zachary Bryant - Primary
 //temporarily using test values
 
-import React, { useEffect, useState } from 'react';
+//global vars/consts
+const cartItems = new Array;
+const totalPrice = 0.00;
 
-//the list of cart items will be passed as a parameter here later
-function Checkout() {
-    //global vars/consts
-    let cartItems = new Array;
-    // let totalPrice = 0.00;
-    // const [cartItems, setCartItems] = useState([]);
-    let totalPrice = 0.00;
+//test values
+const menuItem = {
+    name: nam,
+    price: pric
+};
 
-    class MenuItem {
-        constructor(nam, pric) {
-            this.name = nam;
-            this.price = pric;
-        }
-    }
-
-    //ensures the functions are called after Checkout() function is mounted
-    useEffect(() => {
-        receiveCart();
-        //calcTotal();
-    }, []);
-
-    //test values
-    const food1 = new MenuItem("steak", 20.00);
-    const food2 = new MenuItem("chicken", 12.12);
-    const food3 = new MenuItem("super food", 100.50);
-
-    //this function will receive a list/array of items from the cart when entering checkout
-    function receiveCart() {
-        //logic to input the list into the global constant, use push method OR simply assign it to cartItems
-        cartItems.push(food1);
-        cartItems.push(food2);
-        cartItems.push(food3);
-
-        // const food1 = { name: "steak", price: 20.00 };
-        // const food2 = { name: "chicken", price: 12.12 };
-        // const food3 = { name: "super food", price: 100.50 };
-
-        // setCartItems([food1, food2, food3]);
-
-        calcTotal();
-
-        //this functions similarly to a loop to properly sum the prices
-        //note: using for loop with a state can have unintended consequences with the critical region
-        // const total = cartItems.reduce((acc, item) => acc + item.price, 0);
-        // setTotalPrice(total);
-
-        // console.log(totalPrice);
-
-        for (let i = 0; i < cartItems.length; i++) {
-            console.log(cartItems[i].name);
-        }
-
-        // console.log("total: " + total);
-        console.log("totalPrice: " + totalPrice);
-    }
-
-    //below will calculate the total price from the list of selections in the cart
-    function calcTotal() {
-        //logic to cycle through each item, add its price to the total
-        //for loop
-        for (let i = 0; i < cartItems.length; i++) {
-            totalPrice += cartItems[i].price;
-            // setTotalPrice(totalPrice + cartItems[i].price);
-        }
-
-        console.log("totalPrice: " + totalPrice);
-    }
-
-    return (
-        <div>
-            <h1>Checkout</h1>
-            <p>Total: {totalPrice}</p>
-            <ul>
-                {cartItems.map((item, index) => (
-                    <li key={index}>{item.name} - {item.price}</li>
-                ))}
-                <li>Total: {totalPrice}</li>
-            </ul>
-        </div>
-    );
+function MenuItem(nam, pric){
+    this.name = nam;
+    this.price = pric;
 }
 
-export default Checkout;
+var food1 = new MenuItem("steak", 20.00);
+var food2 = new MenuItem("chicken", 12.12);
+var food3 = new MenuItem("super food", 100.50);
+
+//this function will receive a list/array of items from the cart when entering checkout
+function receiveCart(){
+    //logic to input the list into the global constant, use push method
+    cartItems.push(food1);
+    cartItems.push(food2);
+    cartItems.push(food3);
+}
+
+//below will calculate the total price from the list of selections in the cart
+function calcTotal(){
+    //logic to cycle through each item, add its price to the total
+    //for loop
+    for (i = 0; i < cartItems.length; i++){
+        totalPrice += cartItems[i].price;
+    }
+
+    console.log(totalPrice);
+}
+
