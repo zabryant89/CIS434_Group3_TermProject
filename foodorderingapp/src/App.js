f80fa65738837822fa8cb7a4ba9fb7c2a73540e6
zabryant89 <zachary.a.bryant1@gmail.com>
2023-04-18 12:54:14 -0400

    Added various features
    VIP check (hardcoded for now)
    Return to menu
    VIP registration (dummy function for now)
    Process payment (dummy function for now)
    
    I will work on the payment processing later (using hardcoded numbers for the time being, one card # will always have available funds, the other will be denied).

--- foodorderingapp/src/App.js
+++ foodorderingapp/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Checkout from './checkout';
@@ -6,28 +6,31 @@ import Checkout from './checkout';
 function App() {
   const [showCheckout, setShowCheckout] = useState(false);
 
-  const handleCheckout = () =>{
-    setShowCheckout(true);
+  const handleCheckout = () => {
+    setShowCheckout(!showCheckout);
   };
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button onClick={handleCheckout}>I'm a button</button>
-      </header>
-      {showCheckout && <Checkout />}
+      {showCheckout ? (
+        <Checkout handleCheckout={handleCheckout} />
+      ) : (
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <button onClick={handleCheckout}>I'm a button</button>
+        </header>
+      )}
     </div>
   );
 }
--- foodorderingapp/src/checkout.js
+++ foodorderingapp/src/checkout.js
@@ -4,10 +4,14 @@
 //      -hard code use cases: card # 1234 always has enough funds, card # 6789 always declined
 //      -legitimate UI
 import React, { useState, useEffect } from 'react';
+import './checkout.css';
 
-function Checkout() {
+function Checkout({ handleCheckout }) {
     const [cartItems, setCartItems] = useState([]);
     const [totalPrice, setTotalPrice] = useState(0.0);
+    const [isVisible, setIsVisible] = useState(true);
+    const [userLoggedIn, setUserLoggedIn] = useState(true);
+    
 
     class MenuItem {
         constructor(nam, pric) {
@@ -43,16 +47,48 @@ function Checkout() {
         calcTotal();
     }, [cartItems]);
 
+    function prompt() {
+        //logic to prompt user for VIP register/login
+        //if logged in already, do not prompt, skip
+        if (!userLoggedIn) {
+            payment();
+        }
+        else {
+            //hide checkout window
+            //i dont know a better way to do this right now
+            setIsVisible(false);
+        }
+    }
+
+    function payment() {
+        //logic to process VIP/card payment
+    }
+
+    function vipLogin(){
+        //logic to send to the vip login screen then continue the payment
+    }
+
     return (
-        <div>
-            <h1>Checkout</h1>
-            <ul>
-                {cartItems.map((item, index) => (
-                    <li key={index}>{item.name}: ${item.price.toFixed(2)}</li>
-                ))}
-            </ul>
-            <p>Total price: ${totalPrice.toFixed(2)}</p>
-        </div>
+        <>
+            <div className={`checkout ${isVisible ? '' : 'hidden'}`}>
+                <h1 className='checkout__item__title'>Checkout</h1>
+                <ul className='checkout__item__list'>
+                    {cartItems.map((item, index) => (
+                        <li key={index} className='checkout__item'>{item.name}: ${item.price.toFixed(2)}</li>
+                    ))}
+                </ul>
+                <p className='checkout__final__price'>Total price: ${totalPrice.toFixed(2)}</p>
+                <button onClick={handleCheckout} className='buttons'>Return to main</button>
+                <button className='buttons' onClick={prompt}>Proceed to Payment</button>
+            </div>
+            <div className={`vip ${!isVisible ? '' : 'hidden'}`}>
+                <h1>You're not a VIP Member!</h1>
+                <p>VIP Members gain points on every purchase!</p>
+                <p>Use points to purchase items and receive discounts!</p>
+                <button className='buttons' onClick={payment}>Continue as Guest</button>
+                <button className='buttons' onClick={vipLogin}>Register!</button>
+            </div>
+        </>
     );
 }
 
