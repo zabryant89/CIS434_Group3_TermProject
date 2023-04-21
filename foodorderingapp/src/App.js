3e6d9035f668cf5cf528faad80b6da62779f98b4
zabryant89 <zachary.a.bryant1@gmail.com>
2023-04-21 15:34:49 -0400

    More Checkout Stuff
    Integrated with Sarah's GUI (I merged the main branch with mine so I could work within it).  Just note this code needs cleaned up quite a bit.  There's old commented code in there, I need to add some comments to certain functions, and I also need to clear some of the alerts as they were for debugging/placeholder purposes.
    
    Overall there is one portion of functionality that needs fixed (which is easy: no VIP login).
    
    Now I just need to link everything with the VIP, Orders, and potentially the database.  Once those are complete, My area is done.

--- foodorderingapp/src/App.js
+++ foodorderingapp/src/App.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Checkout from './checkout';
 import Nav from "./NavbarComp/Nav.js";
--- foodorderingapp/src/checkout.js
+++ foodorderingapp/src/checkout.js
@@ -6,12 +6,15 @@
 import React, { useState, useEffect } from 'react';
 import './checkout.css';
 
-function Checkout({ handleCheckout }) {
+function Checkout({ setCheckoutFalse, setCheckoutTrue }) {
     const [cartItems, setCartItems] = useState([]);
     const [totalPrice, setTotalPrice] = useState(0.0);
+    const [returning, setReturning] = useState(true); //if not returning, we are here!
     const [isVisible, setIsVisible] = useState(true);
     const [userLoggedIn, setUserLoggedIn] = useState(true);
-    
+    const [paying, setPaying] = useState(false);
+    const [cardNumber, setCardNumber] = useState('');
+
 
     class MenuItem {
         constructor(nam, pric) {
@@ -45,13 +48,14 @@ function Checkout({ handleCheckout }) {
     useEffect(() => {
         console.log("Cart items changed, recalculating total");
         calcTotal();
-    }, [cartItems]);
+        showCheckout();
+    }, [cartItems]); //dependency on cartItems, any changes to cartItems re-apply this useEffect
 
     function prompt() {
         //logic to prompt user for VIP register/login
         //if logged in already, do not prompt, skip
         if (!userLoggedIn) {
-            payment();
+            paymentScreen();
         }
         else {
             //hide checkout window
@@ -60,17 +64,58 @@ function Checkout({ handleCheckout }) {
         }
     }
 
-    function payment() {
+    function paymentScreen() {
         //logic to process VIP/card payment
+        //user logged in, can use VIP points
+        //user not logged in, card only
+        if (userLoggedIn) {
+            alert("Thank you for being a member and for your payment!");
+            setPaying(true);
+        }
+        else {
+            alert("Thank you for your payment!");
+        }
+        setIsVisible(false);
     }
 
-    function vipLogin(){
+    function pay() {
+        //check the val for a proper value:
+        var val = document.getElementById('cardNumber').value;
+        if (val.trim() === '') {
+            alert("Please enter a card number");
+            return;
+        }
+
+        //handle the payment
+        if (val == 1234) {
+            alert("Payment success!");
+            hideCheckout();
+        }
+        else if (val === 6789) {
+            alert("Payment failed, please try again!");
+        }
+        else {
+            alert("Error: Please enter a proper card number!");
+        }
+    }
+
+    function vipLogin() {
         //logic to send to the vip login screen then continue the payment
+        alert("Need to call the login for VIP members here!")
+    }
+
+    function hideCheckout() {
+        setReturning(true);
+        setPaying(false);
+    }
+
+    function showCheckout() {
+        setReturning(false);
     }
 
     return (
         <>
-            <div className={`checkout ${isVisible ? '' : 'hidden'}`}>
+            <div className={`checkout ${(isVisible && !returning) ? '' : 'hidden'}`}>
                 <h1 className='checkout__item__title'>Checkout</h1>
                 <ul className='checkout__item__list'>
                     {cartItems.map((item, index) => (
@@ -78,15 +123,31 @@ function Checkout({ handleCheckout }) {
                     ))}
                 </ul>
                 <p className='checkout__final__price'>Total price: ${totalPrice.toFixed(2)}</p>
-                <button onClick={handleCheckout} className='buttons'>Return to main</button>
+                <button onClick={() => { setCheckoutFalse(); hideCheckout(); }} className='buttons'>Return to order</button>
                 <button className='buttons' onClick={prompt}>Proceed to Payment</button>
             </div>
-            <div className={`vip ${!isVisible ? '' : 'hidden'}`}>
+            <div className={`vip ${(!isVisible && !returning) ? '' : 'hidden'}`}>
                 <h1>You're not a VIP Member!</h1>
                 <p>VIP Members gain points on every purchase!</p>
                 <p>Use points to purchase items and receive discounts!</p>
-                <button className='buttons' onClick={payment}>Continue as Guest</button>
+                <button className='buttons' onClick={paymentScreen}>Continue as Guest</button>
                 <button className='buttons' onClick={vipLogin}>Register!</button>
+            </div>
+            <div className={`payment ${(paying && userLoggedIn && !returning) ? '' : 'hidden'}`}>
+                <h1>Payment</h1>
+                <p>Points: ---</p>
+                <form onSubmit={() => { setCheckoutFalse(); hideCheckout(); }}>
+                    <p>
+                        <label htmlFor='cardNumber'>Enter your card number</label>
+                        <input type='number' id='cardNumber' />
+                    </p>
+                    <button className='buttons' type='button' onClick={() => { setCheckoutFalse(); hideCheckout(); }}>Return to order</button>
+                    <button className='buttons' type='button' onClick={pay}>Pay Now</button>
+                </form>
+                {/* <p>Card number: <input type='number' id='cardNumber'></input></p>
+                <label for='cardNumber'>Enter your card number</label>
+                <button className='buttons' onSubmit={() => {setCheckoutFalse(); hideCheckout();}}>Return to order</button>
+                <button className='buttons' onClick={pay()}>Pay Now</button> */}
             </div>
         </>
     );
