22bd1af45e2a752c3c3e83f57d46b053f7657170
jodjordje <djordjevicj@outlook.com>
2023-04-22 19:32:17 -0400

    Added UI for VIP section

--- foodorderingapp/src/NavbarComp/VIP.js
+++ foodorderingapp/src/NavbarComp/VIP.js
@@ -1,9 +1,13 @@
-
+import './Style/VIP.css'
 export default function VIP(){
 
 
     return(
-   <h1>VIP!! GET GREAT DEALS!!!!</h1>
+   <div className='signInBox'>
+    <h1 align = "center"><b>Username</b><input className = "signInInfo" id ="username" minlength = "5" maxlength = "10"></input></h1>
+    <h1 align = "center"><b>Password</b><input className = "signInInfo" id ="password" type = "password" minlength = "8" maxlength = "20"></input></h1>
+    <button type = "submit" className = "signInButton">Sign In</button>
+   </div>
     );
 
 }
